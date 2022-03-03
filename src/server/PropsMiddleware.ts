7fd6f641def85f9d20a258878f3d9b02c231ffba
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2022-03-03 23:52:57 +0100

    Add params to PropsMiddleware

--- src/server/PropsMiddleware.ts
+++ src/server/PropsMiddleware.ts
@@ -2,9 +2,11 @@ import { Context } from './Context';
 import { GsspResponse } from './GsspResponse';
 import { GsspMiddleware } from './Middleware';
 
-export function PropsMiddleware<Props>(getProps: (ctx: Context) => Promise<Props>): GsspMiddleware {
+export function PropsMiddleware<Props, Params>(
+  getProps: (ctx: Context, params: Params) => Promise<Props>
+): GsspMiddleware {
   return async (ctx) => {
-    const props = await getProps(ctx);
+    const props = await getProps(ctx, ctx.body as any);
     return GsspResponse.create({
       props,
     });
